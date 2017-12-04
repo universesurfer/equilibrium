fd0471f3df5fc8de2f5b0e450f1f4f1b58b15e6e
Nicholas Rodman <gitnicholas@gmail.com>
2017-12-03 21:44:57 -0800

    new hero, category tiles added

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
+//Require Dependencies
 const express = require("express");
 const path = require("path");
 const http = require("http");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const flash = require("connect-flash");
+const logger = require("morgan");           //logs our requests to the console
+const app = express();
+
+//Connect MongoDB
+mongoose.connect("mongodb://localhost:27017/equilibrium");
+
 
 //Get our API routes
 const api = require("./server/routes/api");
 
-const app = express();
+
+app.use(logger('dev'));  //log every request to the console
 
 //Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.set('view engine', 'ejs');  //setup ejs for templating
+
+
+//Setup passport
+// app.use(session({
+//     secret: "ilovescotch"      //session secret
+// }));
+app.use(passport.initialize());
+app.use(passport.session());   //persistent login sessions
+app.use(flash());     //use connect-flash for flash messages stored in session
 
 //Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule

be410441784fcad26e63dfa6efceb807c89a884e
Nicholas Rodman <gitnicholas@gmail.com>
2017-12-19 12:11:33 -0800

    added Reformation and Amour Vert clothing tiles

---
+++ src/app/category-pages/outdoors/outdoors.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-outdoors',
+  templateUrl: './outdoors.component.html',
+  styleUrls: ['./outdoors.component.css']
+})
+export class OutdoorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

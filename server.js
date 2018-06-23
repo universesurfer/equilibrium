b1acf5ef7939f3f6d1f10687297176b13d964a1e
Nicholas Rodman <gitnicholas@gmail.com>
2018-06-23 14:59:23 -0700

    modifying app.listen to fix port binding

--- server.js
+++ server.js
@@ -83,4 +83,4 @@ app.set('port', port);
 const server = http.createServer(app);
 
 // Listen on provided port, on all network interfaces
-server.listen(port, () => console.log(`API running on localhost: ${port}`));
+server.listen('port', () => console.log(`API running on localhost: ${port}`));
